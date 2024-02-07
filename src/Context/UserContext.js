0a567309e4cbd24ada51e98b9ca5bb10b2e9e1ab
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-02-07 09:25:29 +0500

    Enviroment setup and basic file structure

---
+++ src/Context/UserContext.js
@@ -0,0 +1,8 @@
+import { React, createContext } from 'react';
+
+// Step 1---- > Creating context
+
+// creating a context and assigned to user
+const UserContext = createContext();
+
+export default UserContext;
